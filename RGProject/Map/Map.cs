378b349c3847f7d8cb493cc5e3b1e931d4fa22f7
carter oblivion <nodomatyi@gmail.com>
2024-02-27 13:31:44 +0100

    Add project files.

---
+++ RGProject/Map/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyRPG.Map
+{
+    internal class Map
+    {
+        // Map is a 3D matrix of 'Tile
+        // Z0: Decorations and 'Terrain' Tiles
+        // Z1: Enemy Tiles
+        // Z2: Loot and Servant Tiles
+        // Z3: Player Tile
+        public Map() { }
+    }
+
+}
---
+++ RGProject/Map/Tile/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyRPG.Map.Tile
+{
+    enum TileType
+    {
+        Terrain,
+        Enemy,
+        Loot,
+        Servant,
+        Player
+    }
+    internal class Tile
+    {
+
+        public Tile(string TileName, TileType TileType)
+        {
+            ID = Guid.NewGuid();
+            Name = TileName;
+            Type = TileType;
+        }
+
+        public Guid ID { get; }
+        public string Name { get; }
+        public TileType Type { get; }
+
+    }
+}
