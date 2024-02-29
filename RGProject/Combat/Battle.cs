9ddc4ae562129435e20a44c5a5d1f939758ebaf2
carter oblivion <nodomatyi@gmail.com>
2024-02-29 13:17:03 +0100

    publicize the interfaces

--- RGProject/Combat/Battle.cs
+++ RGProject/Combat/Battle.cs
@@ -7,13 +7,27 @@ using System.Threading.Tasks;
 
 namespace FantasyRPG.Combat
 {
-    internal interface IBattle : IDisposable
+    public interface IBattle : IDisposable
     {
         Guid Id { get; }
-        ICharacter Character { get; }
+        ICharacter Hero { get; }
         ICharacter Enemy { get; }
-
+        void Initiate();
         void Turn(); // Advance to next turn
         void Defeat(ICharacter Character);
+    }
+    public class Battle : IBattle
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public ICharacter Hero { get; }
+        public ICharacter Enemy { get; }
+        public ICharacter CurrentlyAttacking { get; set; }
+        public void Initiate() { }
+        public void Turn() { }
+        public void Defeat(ICharacter Character) { }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
     }
 }
