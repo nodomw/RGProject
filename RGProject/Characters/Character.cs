b5aca11255a85b7253edb4f866fff29e933e0f32
carter oblivion <nodomatyi@gmail.com>
2024-02-29 12:34:46 +0100

    Add: Character interface

--- RGProject/Characters/Character.cs
+++ RGProject/Characters/Character.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FantasyRPG.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace FantasyRPG.Characters
 {
-    internal class Character
+    public enum Character
     {
+        Controller,
+        Servant,
+        Enemy,
+    }
+    internal interface ICharacter
+    {
+        Guid Id { get; }
+        string Name { get; }
+        int Level { get; set; }
+        double Health { get; set; }
+        double Damage { get; set; }
+        double Resistance { get; set; }
+        double Dodge { get; set; }
+        List<IPotion> Potions { get; set; }
+        List<IWeapon> Weapons { get; set; }
+        IWeapon CurrentWeapon { get; set; }
+
+        int LevelUp();
+        int Attack(int Damage);
+        int TakeDamage(int Damage);
+        void Interact(dynamic Thing);
     }
 }
