fc6673ae0e57665ee32172857c8273075aa25170
wintea <nodomatyi@gmail.com>
2024-03-01 20:54:27 +0100

    Base character implementation

--- RGProject/Characters/Character.cs
+++ RGProject/Characters/Character.cs
@@ -1,4 +1,5 @@
 ﻿using FantasyRPG.Items;
+using FantasyRPG.Map;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace FantasyRPG.Characters
 {
-    public enum Character
+    public enum CharacterType
     {
         Controller, // Player
         Servant,
         Enemy
+    }
+    public enum UpgradableTraits
+    {
+        Health,
+        Resistance,
+        Dodge
     }
     public enum Hero
     {
@@ -21,23 +28,33 @@ namespace FantasyRPG.Characters
     {
 
     }
-    public interface ICharacter
+    abstract class Character
     {
-        Guid Id { get; }
-        string Name { get; }
-        int Level { get; set; }
-        double Health { get; set; }
-        double Damage { get; set; }
-        double Resistance { get; set; }
-        double Dodge { get; set; }
-        Character Type { get; }
-        List<IPotion> Potions { get; set; }
-        List<IWeapon> Weapons { get; set; }
-        IWeapon CurrentWeapon { get; set; }
+        public Guid Id { get; }
+        public string Name { get; }
+        public int Level { get; set; }
+        public int XP { get; set; } // figure out xp amount for each level
+        public double Health { get; set; }
+        public double Resistance { get; set; }
+        public double Dodge { get; set; }
+        public CharacterType Type { get; }
+        public List<IPotion> Potions { get; set; }
+        public List<IWeapon> Weapons { get; set; }
+        public IWeapon CurrentWeapon { get; set; }
 
-        int LevelUp();
-        int Attack(int Damage);
-        int TakeDamage(int Damage);
-        void Interact(dynamic Thing);
+        public int Attack(int Damage) => CurrentWeapon.Damage * (Level / 100); // should increase with level so like level 12 should give you a 1.2% passive damage increase
+        public int LevelUp()
+        {
+            Level += 1;
+
+            return Level;
+        }
+        public int TakeDamage(int Damage)
+        {
+            // TODO: calculate dodge probability chance
+            Health -= Damage * (1 - (Resistance / 100));
+
+            return Damage;
+        }
     }
 }
