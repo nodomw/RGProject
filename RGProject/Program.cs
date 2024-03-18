a7c4998f1eed53323794e873522ad541f4270b99
wintea <nodomatyi@gmail.com>
2024-03-18 23:21:17 +0100

    push map-oriented program.cs

--- RGProject/Program.cs
+++ RGProject/Program.cs
@@ -1,48 +1,30 @@
-﻿using FantasyRPG.UI;
-using FantasyRPG.Map;
+﻿using FantasyRPG.Map;
 using Spectre.Console;
-using FantasyRPG.Combat;
 using FantasyRPG.Map.Tiles;
-using FantasyRPG.Characters;
 using RGProject.Characters.Heroes;
 using RGProject.Characters.Enemies;
 
-var enemy = new Emperor("Lajos");
-var font = FigletFont.Load("ANSI Shadow.flf");
-var font2 = FigletFont.Load("Cyberlarge.flf");
-string charclass = "";
-string charname = "";
-string[] temp = new string[2];
+Enemy v = new("vampire", new Vampire("gino juno"));
+Player p = new("joni", new Assassin("joni"));
+Empty e = new();
+Terrain t = new();
 
-var tempmenu = new Menu(new Battle(new Hero("john doe"), new Emperor("Lajos")));
-temp = tempmenu.ShowCharSelection();
-charname = temp[0];
-charclass = temp[1];
+Map m1 = new Map("map 1", new ITile[,] {
+	// when making an actual map b sure to create actual instances instead of substituting stuff in
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, p, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e },
+	{ e, e, e, e, e, e, e, e, e, e }
+});
 
-ICharacter hero = charclass switch
-{
-	"Warrior" => new Warrior(charname),
-	"Mage" => new Mage(charname),
-	"Elf" => new Elf(charname),
-	"Assassin" => new Assassin(charname),
-	"Paladin" => new Paladin(charname),
-	"Hero" => new Hero(charname),
-	"Hunter" => new Hunter(charname),
-	"Ninja" => new Ninja(charname),
-	_ => throw new ArgumentOutOfRangeException()
-};
+System.Console.WriteLine($"showing map '{m1.Name}'");
+m1.DrawFull(DrawCriteria.DisplayCharacter);
+m1.DrawGrid();
 
-Battle battle = new Battle(hero, enemy);
-var menu = new Menu(battle);
-battle.Turn();
-
-/*Console.Clear();
-AnsiConsole.Write(new FigletText(font2, "Welcome to").Centered().Color(Color.White));
-AnsiConsole.Write(new FigletText(font, "Fantasy Frontiers").Centered().Color(Color.Red));
-
-AnsiConsole.Write(new Markup("[red]Press any key to continue.....[/]"));
-Console.ReadKey();
-
-temp = menu.Show();
-charname = temp[0];
-charclass = temp[1];*/
+AnsiConsole.Write(m1.GetTileByPosition(new TilePosition(-1, -1)).DisplayCharacter);
