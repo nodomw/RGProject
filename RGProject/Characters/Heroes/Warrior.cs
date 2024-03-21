e60926d2a854bbf61a23b366330bce1dac84c807
wintea <nodomatyi@gmail.com>
2024-03-21 22:41:33 +0100

    jeaneral fixes

--- RGProject/Characters/Heroes/Warrior.cs
+++ RGProject/Characters/Heroes/Warrior.cs
@@ -46,6 +46,6 @@ public class Warrior : ICharacter
 	public double TempDEF { get; set; } // %
 	public double TempCombo { get; set; } // %
 	public double TempCATK { get; set; } // %
-	public List<Item> Items { get; set; }
+	public List<Item> Items { get; set; } = new List<Item>();
 	public IWeapon Weapon { get; set; }
 }
