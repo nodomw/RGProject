a623f19707ee63b17d340f2cdb19c4416c725f17
wintea <nodomatyi@gmail.com>
2024-03-29 22:56:51 +0100

    Dead icon for dead enemies

--- RGProject/Map/Tiles/Enemy.cs
+++ RGProject/Map/Tiles/Enemy.cs
@@ -5,13 +5,19 @@ using Spectre.Console;
 namespace FantasyRPG.Map.Tiles;
 
 
-public class Enemy(ICharacter character) : ITile, IFightable, ICharacterTile
+public class Enemy : ITile, IFightable, ICharacterTile
 {
+
+    public Enemy(ICharacter character)
+    {
+        Character = character;
+        Character.Parent = this;
+    }
     public Guid Id { get; } = Guid.NewGuid();
     public TileType Type { get; } = TileType.Enemy;
-    public ICharacter Character { get; } = character;
+    public ICharacter Character { get; }
     public TilePosition Position { get; set; }
-    public bool Passable { get; set; } = false;
+    public bool Passable { get; set; } = true;
     public string Name { get; set; } = "Enemy";
     public Markup DisplayCharacter { get; set; } = new Markup("[red]![/]");
     public void Interact(Battle battle) => battle.Turn();
--- RGProject/Map/Tiles/Player.cs
+++ RGProject/Map/Tiles/Player.cs
@@ -3,12 +3,17 @@ using Spectre.Console;
 
 namespace FantasyRPG.Map.Tiles
 {
-    public class Player(ICharacter character) : ITile, ICharacterTile
+    public class Player : ITile, ICharacterTile
     {
+        public Player(ICharacter character)
+        {
+            Character = character;
+            Character.Parent = this;
+        }
         public Guid Id { get; } = Guid.NewGuid();
         public TileType Type { get; } = TileType.Player;
         public string Name { get; set; } = "Hero";
-        public ICharacter Character { get; } = character;
+        public ICharacter Character { get; }
         public bool Passable { get; set; } = true;
         public TilePosition Position { get; set; }
         public Markup DisplayCharacter { get; set; } = new Markup("[bold white]@[/]");
